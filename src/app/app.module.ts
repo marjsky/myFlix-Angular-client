e3e04a0af99e5336a2944b9aaeff58606903738d
marjsky <67877013+marjsky@users.noreply.github.com>
2022-10-23 16:20:00 -0400

    add few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,9 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DirectorComponent } from './director/director.component';
+import { GenreComponent } from './genre/genre.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
 
 const appRoutes: Routes=[
   { path: 'welcome', component: WelcomePageComponent },
@@ -38,7 +41,10 @@ const appRoutes: Routes=[
     UserLoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
-    ProfileComponent
+    ProfileComponent,
+    DirectorComponent,
+    GenreComponent,
+    SynopsisComponent
   ],
   imports: [
     BrowserModule,

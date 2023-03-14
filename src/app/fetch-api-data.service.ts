5efa135bce6f45eac56441c7d63a6ad068543b17
marjsky <67877013+marjsky@users.noreply.github.com>
2023-03-14 18:31:59 -0400

    remove web service of render and use railway

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://movie-api-5jsk.onrender.com/';
+const apiUrl = 'https://movie-api-production-3c8a.up.railway.app/';
 
 const token = localStorage.getItem('token');
 
